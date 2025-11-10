bad63310adb44c7c2827359646d609c57d16deb5
Sumezi <mchr270902@gmail.com>
2025-11-10 19:15:50 +0100

    fix parameter sampling and initialization

--- environments/harmonic_oscillator.py
+++ environments/harmonic_oscillator.py
@@ -40,41 +40,39 @@ class HarmonicOscillator(EnvironmentBase):
     def sample_params(self, batch_size, mode, ts, key):
         omega_key, zeta_key, args_key = jrandom.split(key, 3)
         if mode == "constant":
-            omegas = jnp.ones((batch_size, ts.shape[0]))
-            zetas = jnp.zeros((batch_size, ts.shape[0]))
+            omegas = jnp.ones((batch_size))
+            zetas = jnp.zeros((batch_size))
         elif mode == "different":
-            omegas = jrandom.uniform(omega_key, (batch_size,), minval=0.0, maxval=2.0)[:, None] * jnp.ones((batch_size, ts.shape[0]))
-            zetas = jrandom.uniform(zeta_key, (batch_size,), minval=0.0, maxval=1.5)[:, None] * jnp.ones((batch_size, ts.shape[0]))
+            omegas = jrandom.uniform(omega_key, shape=(batch_size,), minval=0.0, maxval=2.0)
+            zetas = jrandom.uniform(zeta_key, shape=(batch_size,), minval=0.0, maxval=1.5)
         elif mode == "changing":
-            decay_factors = jrandom.uniform(args_key, (batch_size, 2), minval=0.98, maxval=1.02)
-            init_omegas = jrandom.uniform(omega_key, (batch_size,), minval=0.5, maxval=1.5)
-            init_zetas = jrandom.uniform(zeta_key, (batch_size,), minval=0.0, maxval=1.0)
+            decay_factors = jrandom.uniform(args_key, shape=(batch_size, 2), minval=0.98, maxval=1.02)
+            init_omegas = jrandom.uniform(omega_key, shape=(batch_size,), minval=0.5, maxval=1.5)
+            init_zetas = jrandom.uniform(zeta_key, shape=(batch_size,), minval=0.0, maxval=1.0)
             omegas = jax.vmap(lambda o, d, t: o * (d ** t), in_axes=[0, 0, None])(init_omegas, decay_factors[:, 0], ts)
             zetas = jax.vmap(lambda z, d, t: z * (d ** t), in_axes=[0, 0, None])(init_zetas, decay_factors[:, 1], ts)
         return omegas, zetas
 
     def initialize_parameters(self, params, ts):
         omega, zeta = params
-        A = jax.vmap(lambda o, z: jnp.array([[0, 1], [-o, -z]]))(omega, zeta)
-        self.A = diffrax.LinearInterpolation(ts, A)
+        self.A = jnp.array([[0, 1], [-omega, -zeta]])
         self.b = jnp.array([[0.0, 1.0]]).T
         self.G = jnp.array([[0, 0], [0, 1]])
-        self.V = self.process_noise*self.G
+        self.V = self.process_noise * self.G
         self.C = jnp.eye(self.n_var)[:self.n_obs]
-        self.W = self.obs_noise*jnp.eye(self.n_obs)
+        self.W = self.obs_noise * jnp.eye(self.n_obs)
 
     def drift(self, t, state, args):
-        return self.A.evaluate(t) @ state + self.b @ args
+        return self.A @ state + self.b @ args
 
     def diffusion(self, t, state, args):
         return self.V
 
     def fitness_function(self, state, control, target, ts):
-        target = jnp.squeeze(target)
-        x_d = jnp.stack([target, jnp.zeros_like(target)], axis=1)
-        u_d = jax.vmap(lambda t, xd: -jnp.linalg.pinv(self.b) @ self.A.evaluate(t) @ xd)(ts, x_d)
-        costs = jax.vmap(
-            lambda _state, _u, _xd, _ud: (_state - _xd) @ self.Q @ (_state - _xd) + (_u - _ud) @ self.R @ (_u - _ud)
+        x_d = jnp.stack([target, jnp.zeros_like(ts)], axis=1)
+        u_d = jax.vmap(lambda _x_d: -jnp.linalg.pinv(self.b) @ self.A @ _x_d)(x_d)
+        costs = jax.vmap(lambda _state, _u, _x_d, _u_d: 
+            (_state - _x_d).T @ self.Q @ (_state - _x_d) + (_u - _u_d) @ self.R @ (_u - _u_d)
         )(state, control, x_d, u_d)
         return jnp.mean(costs)
 
